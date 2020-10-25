d8e20f1d1d23ef9e9dfe60d38eb4ff70b16d788f
Mauricio Fullana <mfullana4@gmail.com>
2020-10-25 00:02:57 -0300

    Basic BroadcastReceiver example

--- app/src/main/java/com/example/androidarchitecture/SecondActivity.java
+++ app/src/main/java/com/example/androidarchitecture/SecondActivity.java
@@ -2,13 +2,31 @@ package com.example.androidarchitecture;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 public class SecondActivity extends AppCompatActivity {
 
+    MyReceiver myReceiver;
+
+    public static final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        myReceiver = new MyReceiver();
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(SMS_RECEIVED);
+        registerReceiver(myReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (myReceiver != null) {
+            unregisterReceiver(myReceiver);
+        }
     }
 }
